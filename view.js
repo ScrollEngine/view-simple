1997a61840ba6f29013fc98015a63c9a464e5947
Richard Key <rich@busyrich.com>
2014-04-27 22:08:32 -0700

    Removed everything but the most basic components

--- view.js
+++ view.js
@@ -3,15 +3,14 @@ var exphbs = require('express3-handlebars');
 module.exports = function(app) {
   hbs = exphbs.create({
     extname: '.hbs',
-    defaultLayout: app.config.view.path + '/views/layouts/main',
-    helpers: require('./helpers')(app.config.view.helpers || {})
+    defaultLayout: app.config.view.path + '/views/layouts/main'
   });
 
   app.locals.title = "Scroll - Simple";
   app.locals.nav = [
     {text:'Page One',url:'/page1'},
     {text:'Page Two',url:'/page2'},
-    {icon:'home-outline',url:'/'},
+    {text:'Home',url:'/'},
     {text:'Page Three',url:'/page3'},
     {text:'Page Four',url:'/page4'}
   ];
